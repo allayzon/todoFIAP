4107ede8a452a65566457f337bc44d0bd1c2ecad
allayzon <allyson.g.d.santos@gmail.com>
2022-05-04 13:49:52 -0300

    Criando função que deleta uma tarefa

--- js/script.js
+++ js/script.js
@@ -11,3 +11,54 @@ inputBox.onkeyup = _ => {
         addBtn.classList.remove('active')
     }
 }
+
+showTasks()
+
+addBtn.onclick = _ => {
+    let userData = inputBox.value
+    let getLocalStorage = localStorage.getItem('New Todo') //Esta variável vai pegar o armazenamento local
+    if(getLocalStorage == null) { //Vamos verificar se o armazenamento local for nulo
+        ListArr = [] //Vamos criar um array vazio
+    }else{
+        ListArr = JSON.parse(getLocalStorage) //Estamos empurrando ou adicionando dados do usuário à nossa lista
+    }
+    ListArr.push(userData) //Empurrando para dentro do array os dados do usuário
+    localStorage.setItem('New Todo', JSON.stringify(ListArr)) //Estamos transformando um objeto js em um fragmento js
+    showTasks()
+}
+
+//Função para adicionar tarefas dentro da lista 
+function showTasks() {
+    let getLocalStorage = localStorage.getItem('New Todo')
+    if(getLocalStorage == null) {
+        ListArr = []
+    }else{
+        ListArr = JSON.parse(getLocalStorage)
+    }
+    const pendingNum = document.querySelector('.pendingNum')
+    pendingNum.textContent = ListArr.length
+
+    if(ListArr.length > 0) { //Se o tamanho do array for maior que 0
+        deleteAllBtn.classList.add('active') //Ativa a classe active
+    }else{
+        deleteAllBtn.classList.remove('active') //Desativa a classe active
+    }
+
+    let newLiTag = ''
+    addBtn.classList.remove('active')
+    ListArr.forEach((element, index) => {
+        newLiTag += `<li>${element}<span onclick="deleteTask(${index})">apagar</span></li>`
+    })
+    todoList.innerHTML = newLiTag //Adicionando uma nova li dentro da nossa lista no html
+    inputBox.value = '' //Uma vez adicionada a tarefa, deixe o campo entrada em branco
+}
+
+//Função para deletar uma tarefa específica
+function deleteTask(index) {
+    let getLocalStorage = localStorage.getItem('New Todo')
+    ListArr = JSON.parse(getLocalStorage)
+    ListArr.splice(index, 1) //Exclui ou remove um determinado item indexado
+    //?Depois de remover o item, atualize novamente o armazenamento local
+    localStorage.setItem('New Todo', JSON.stringify(ListArr))
+    showTasks()
+}
